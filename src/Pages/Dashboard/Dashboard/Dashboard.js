c232f1789cc465cecfbea41dc44bf2b83cd539cd
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-08 20:48:51 +0600

    up

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -113,10 +113,10 @@ function Dashboard(props) {
                 <Toolbar />
                 <Typography paragraph>
                     <Grid container spacing={2}>
-                        <Grid item xs={8}>
+                        <Grid item xs={12} md={4}>
                             <Calendar></Calendar>
                         </Grid>
-                        <Grid item xs={4}>
+                        <Grid item xs={12} md={8}>
                             <Appoinments></Appoinments>
                         </Grid>
 
