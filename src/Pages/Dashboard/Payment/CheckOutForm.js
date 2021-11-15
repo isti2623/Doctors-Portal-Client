4ab260488008f6b02cfc0f55f19e47a378852878
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-15 18:45:45 +0600

    update

--- src/Pages/Dashboard/Payment/CheckOutForm.js
+++ src/Pages/Dashboard/Payment/CheckOutForm.js
@@ -1,26 +1,98 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from './../../../hooks/useAuth';
+import { CircularProgress } from '@mui/material';
 
-const CheckOutForm = ({ appointment }) => {
+const CheckoutForm = ({ appointment }) => {
+    const { price, patientName, _id } = appointment;
     const stripe = useStripe();
     const elements = useElements();
+    const { user } = useAuth();
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [clientSecret, setClientSecret] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => setClientSecret(data.clientSecret));
+    }, [price]);
+
 
     const handleSubmit = async (e) => {
-
-        if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
-            return;
-        }
-
-        const card = elements.getElement(CardElement);
-
-        if (card == null) {
-            return;
-        }
-
-
         e.preventDefault();
+        if (!stripe || !elements) {
+            return;
+        }
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+        }
+        setProcessing(true);
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        if (error) {
+            setError(error.message);
+            setSuccess('');
+        }
+        else {
+            setError('');
+            console.log(paymentMethod);
+        }
+
+        // payment intent
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: user.email
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setError(intentError.message);
+            setSuccess('');
+        }
+        else {
+            setError('');
+            setSuccess('Your payment processed successfully.')
+            console.log(paymentIntent);
+            setProcessing(false);
+            // save to database
+            const payment = {
+                amount: paymentIntent.amount,
+                created: paymentIntent.created,
+                last4: paymentMethod.card.last4,
+                transaction: paymentIntent.client_secret.slice('_secret')[0]
+            }
+            const url = `http://localhost:5000/appointments/${_id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(payment)
+            })
+                .then(res => res.json())
+                .then(data => console.log(data));
+        }
+
     }
     return (
         <div>
@@ -41,12 +113,18 @@ const CheckOutForm = ({ appointment }) => {
                         },
                     }}
                 />
-                <button type="submit" disabled={!stripe}>
-                    Pay
-                </button>
+                {processing ? <CircularProgress></CircularProgress> : <button type="submit" disabled={!stripe || success}>
+                    Pay ${price}
+                </button>}
             </form>
+            {
+                error && <p style={{ color: 'red' }}>{error}</p>
+            }
+            {
+                success && <p style={{ color: 'green' }}>{success}</p>
+            }
         </div>
     );
 };
 
-export default CheckOutForm;
+export default CheckoutForm;
