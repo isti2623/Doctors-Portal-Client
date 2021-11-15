b1c5d6c7ec60899c1a3c9eaed411633b8e2fea39
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-15 10:06:15 +0600

    up

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const Payment = () => {
+    const { appoinmentId } = useParams();
     return (
         <div>
-            <h2>Please pay for : </h2>
+            <h2>Please pay for : {appoinmentId}</h2>
         </div>
     );
 };
